ddaa4b54d1e14e7ee1ccf699b0423a96cba5faaa
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-05 13:40:05 -0600

    successfully calculates revenue

--- lib/dock.rb
+++ lib/dock.rb
@@ -1,12 +1,32 @@
+require 'pry'
 class Dock
 
   attr_reader :name,
-              :max_rental_time
+              :max_rental_time,
+              :revenue,
+              :boats_rented,
+              :boats_returned
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
+    @revenue = 0
+    @boats_rented = []
+    @boats_returned = []
   end
 
+  def rent(boat_obj, renter_obj)
+    @boats_rented << boat_obj
+  end
+
+  def log_hour
+    @boats_rented.each do |boat|
+      @revenue += boat.price_per_hour
+    end
+  end
+
+  def return(boat_obj)
+    @boats_rented.delete(boat_obj)
+  end
 
 end
