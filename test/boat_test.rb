b7c5a73f173f3bd65eef7e2d5a4d4198b6ca99cc
Amy Leigh Petrie <apetrie9849@gmail.com>
2018-09-05 13:14:00 -0600

    completes iteration 1

--- test/boat_test.rb
+++ test/boat_test.rb
@@ -22,4 +22,27 @@ class TestBoat < Minitest::Test
     assert_equal 20, kayak.price_per_hour
   end
 
+  def test_it_defaults_to_zero_hours_rented
+    kayak = Boat.new(:kayak, 20)
+
+    assert_equal 0, kayak.hours_rented
+  end
+
+  def test_it_increments_hours_rented_when_you_add_hour
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    kayak.add_hour
+
+    assert_equal 3, kayak.add_hour
+  end
+
+  def test_adding_hours_changes_hours_rented
+    kayak = Boat.new(:kayak, 20)
+    kayak.add_hour
+    kayak.add_hour
+    kayak.add_hour
+
+    assert_equal 3, kayak.hours_rented
+  end
+
 end
